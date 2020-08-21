a62b5678de0e6d5cde3a8a456049297d6ddf4569
Lavrik Andrey <lavrik.aw@ya.ru>
2020-08-21 17:15:31 +0300

    module3-task1 фильтрация контента

--- templates/layout.php
+++ templates/layout.php
@@ -52,7 +52,7 @@
         <ul class="nav__list container">
             <?php foreach ($categories as $categorie): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= $categorie ?></a>
+                    <a href="pages/all-lots.html"><?= htmlspecialchars($categorie) ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
