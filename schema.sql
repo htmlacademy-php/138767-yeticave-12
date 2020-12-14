4ca970c84965a7eceda107f42a86667cc2b58ae0
Lavrik <lavrik.aw@ya.ru>
2020-12-14 22:26:25 +0300

    module4-task1 little fixes

--- schema.sql
+++ schema.sql
@@ -2,29 +2,31 @@ CREATE DATABASE yeticave;
 USE yeticave;
 
 CREATE TABLE categories (
-    cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     symbol_code VARCHAR(255)
 );
 
 CREATE TABLE lots (
     lot_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created TIMESTAMP (6),
+    created TIMESTAMP(6),
     lot_name VARCHAR(128),
-    lot_gategory INT NOT NULL,
+    lot_category_id INT NOT NULL,
+    author_user_id INT NOT NULL,
+    winner_user_id INT NOT NULL,
     description VARCHAR(255),
     img_url VARCHAR(255),
     init_price NUMERIC(19, 4),
-    date_of_completion TIMESTAMP (6),
-    step INT(4)
+    completed TIMESTAMP (6),
+    bet_step INT(4)
 );
 
 CREATE TABLE bets (
     bet_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created TIMESTAMP (6),
+    created TIMESTAMP(6),
     price NUMERIC(19, 4),
-    bet_user INT NOT NULL,
-    bet_lot INT NOT NULL
+    bet_user_id INT NOT NULL,
+    bet_lot_id INT NOT NULL
 );
 
 CREATE TABLE users (
@@ -33,14 +35,19 @@ CREATE TABLE users (
     email VARCHAR(128),
     name VARCHAR(128),
     password VARCHAR(128),
-    contacts VARCHAR(128),
-    user_lot INT NOT NULL,
-    user_bet INT NOT NULL
+    contacts VARCHAR(128)
 );
 
-ALTER TABLE lots ADD FOREIGN KEY(lot_gategory) REFERENCES categories(cat_id);
-ALTER TABLE bets ADD FOREIGN KEY(bet_user) REFERENCES users(user_id);
-ALTER TABLE bets ADD FOREIGN KEY(bet_lot) REFERENCES lots(lot_id);
-ALTER TABLE users ADD FOREIGN KEY(user_lot) REFERENCES lots(lot_id);
-ALTER TABLE users ADD FOREIGN KEY(user_bet) REFERENCES bets(bet_id);
+ALTER TABLE lots ADD FOREIGN KEY(lot_category_id) REFERENCES categories(category_id);
+ALTER TABLE lots ADD FOREIGN KEY(author_user_id) REFERENCES users(user_id);
+ALTER TABLE lots ADD FOREIGN KEY(winner_user_id) REFERENCES users(user_id);
+ALTER TABLE bets ADD FOREIGN KEY(bet_lot_id) REFERENCES lots(lot_id);
+
+CREATE INDEX email ON users(email);
+CREATE INDEX created ON bets(created);
+CREATE INDEX created ON lots(created);
+CREATE INDEX description ON lots(description);
+CREATE INDEX lot_name ON lots(lot_name);
+CREATE INDEX name ON categories(name);
+
 
