ad373124a432ce2a4ca60e6c2c7f380e648083d2
Lavrik <lavrik.aw@ya.ru>
2021-02-09 20:50:04 +0300

    module6-task1

--- init.php
+++ init.php
@@ -10,21 +10,23 @@ if (!$link) {
 
 function get_lots_from_db() {
     global $link;
-    $lots_sql = "SELECT
-    lot_name,
-    init_price,
-    img_url,
-    completed,
-    lots.created,
-    name,
-    MAX(bets.price) as current_price
-    FROM lots
-    JOIN categories ON categories.category_id = lots.lot_category_id
-    LEFT JOIN bets ON bets.bet_lot_id = lots.lot_id
-    WHERE completed > NOW()
-    GROUP BY lots.lot_id
-    ORDER BY lots.created DESC;";
     $lots = [];
+    $lots_sql = "SELECT
+        lot_name,
+        lot_id,
+        lot_category_id,
+        init_price,
+        img_url,
+        completed,
+        name,
+        lots.created,
+        MAX(bets.price) as current_price
+        FROM lots
+        JOIN categories ON categories.category_id = lots.lot_category_id
+        LEFT JOIN bets ON bets.bet_lot_id = lots.lot_id
+        WHERE completed > NOW()
+        GROUP BY lots.lot_id
+        ORDER BY lots.created DESC;";
 
     if ($lots_result = mysqli_query($link, $lots_sql)) {
         $lots = mysqli_fetch_all($lots_result, MYSQLI_ASSOC);
@@ -35,12 +37,28 @@ function get_lots_from_db() {
 
 function get_categories_from_db() {
     global $link;
-    $categories_sql = "SELECT category_id, name, symbol_code FROM categories;";
     $categories = [];
+    $categories_sql = "SELECT category_id, name, symbol_code FROM categories;";
 
     if ($categories_result = mysqli_query($link, $categories_sql)) {
         $categories = mysqli_fetch_all($categories_result, MYSQLI_ASSOC);
     }
 
     return $categories;
+}
+
+function get_lot() {
+    global $link;
+    $lot_category_id = filter_input(INPUT_GET, "category");
+    $lot_id = filter_input(INPUT_GET, "lot");
+
+    if ($lot_category_id && $lot_id) {
+        $lot_sql = "SELECT * from lots WHERE lot_id =" . $lot_id . " lot_category =" . $lot_category_id . ";";
+
+        if ($lot_result = mysqli_query($link, $lot_sql)) {
+            return mysqli_fetch_all($lot_result, MYSQLI_ASSOC);
+        }
+    }
+
+    return false;
 }
--- templates/main.php
+++ templates/main.php
@@ -27,7 +27,11 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?= $item["name"] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item["lot_name"] ?></a></h3>
+                    <h3 class="lot__title">
+                        <a class="text-link" href="../lot.php?<?= "category=" . $item["lot_category_id"] . "&lot=" . $item["lot_id"] ?>">
+                            <?= $item["lot_name"] ?>
+                        </a>
+                    </h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
